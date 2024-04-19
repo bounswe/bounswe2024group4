20d513e13f6dd7433298200ac15d0223bf22772e
Fatih Demir <101219059+fatih260@users.noreply.github.com>
2024-04-19 12:13:16 +0300

    templates folder created

--- backend/group4_backend/group4_backend/settings.py
+++ backend/group4_backend/group4_backend/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'group4_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/group4_backend/nba_project/views.py
+++ backend/group4_backend/nba_project/views.py
@@ -1,52 +1,39 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from .models import Profile
-from django.http import HttpResponse
-#class SignUpView(APIView):
-#    def post(self, request):
+
 def sign_up(request):
-    
     if request.method == "POST":
         # Access form data from POST request
         username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         bio = request.POST.get("bio")
-    
-
+        
         print("username: ", username, "email: ", email, "password: ", password, "bio: ", bio)
 
         if User.objects.filter(email=email).exists():
-            return Response({"error": "Email already taken"}, status=status.HTTP_400_BAD_REQUEST)
+            return HttpResponse("Email already taken", status=400)
         elif User.objects.filter(username=username).exists():
-            return Response({"error": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+            return HttpResponse("Username already taken", status=400)
         else:
             # Create user
             user = User.objects.create_user(username=username, email=email, password=password)
-
             print("user created: ", user)
             # Create profile
             profile = Profile.objects.create(user=user, bio=bio)
-
             print("profile created: ", profile)
 
             # Authenticate and login user
             user_login = authenticate(username=username, password=password)
-
-            
             if user_login:
                 login(request, user_login)
-
                 print("user_logged in: ", user_login)
-                return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+                return HttpResponse("User created successfully", status=201)
             else:
-                return Response({"error": "Failed to authenticate user"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    else:
-        return Response({"error": "Request method must be POST"}, status=status.HTTP_400_BAD_REQUEST)
+                return HttpResponse("Failed to authenticate user", status=500)
+    
+    # Render the signup.html template for GET requests
+    return render(request, 'signup.html')
