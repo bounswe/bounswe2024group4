465c29393cdd22d6d1e2d2dc1b4d476e433ee907
Kaan Güneyli <kaanguneyli@Kaan-MacBook-Pro.local>
2024-04-25 20:19:33 +0300

    create post

--- backend/nba_app/models.py
+++ backend/nba_app/models.py
@@ -42,3 +42,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500, blank=False) 
+    #image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
