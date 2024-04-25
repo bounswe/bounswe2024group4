507b18425393a186e6d617eaa8df73349617fdce
Can <mcank2000@gmail.com>
2024-04-25 11:51:33 +0300

    login first template
    login path added to urls.py
    def login added to views.py
    In the settings.py, configuration for database is changed to personal

--- backend/nba_app/urls.py
+++ backend/nba_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import log_in
+
 
 urlpatterns = [
     path('signup/', views.sign_up, name='signup'),
+    path('login/', log_in, name='login'),
+
 ]
--- backend/nba_app/views.py
+++ backend/nba_app/views.py
@@ -35,3 +35,17 @@ def sign_up(request):
     
     # Render the signup.html template for GET requests
     return render(request, 'signup.html')
+
+def log_in(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Logged in successfully", status=200)
+        else:
+            return HttpResponse("Login not successful", status=401)
+
+    return render(request, 'login.html')
