6f970e07a3b654aa2c7faa636a430bd67a363687
Kaan Güneyli <kaanguneyli@Kaan-MacBook-Pro.local>
2024-04-27 02:04:15 +0300

    wikidata api integration for team and player mostly done

--- backend/nba_app/urls.py
+++ backend/nba_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.log_in, name='login'),
     path('feed/', views.feed, name='feed'),
     path('post/', views.post, name='post'),
+    path('search/', views.search, name='search'),
+
 ]
