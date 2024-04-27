749417caf4ac9f85a0bedb38abc9019e37076efe
berat-yilmaz <brtylmzx@gmail.com>
2024-04-27 17:19:59 +0300

    Fix signin page small bug in frontend

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -39,11 +39,11 @@ function SignIn() {
     <div className= "bg-nba min-h-screen bg-top-left">
         <Navbar />
         <main className="container mx-auto mt-40">
-          <div className="signup-form">
+          <div className="signin-form">
             <form onSubmit={handleSubmit} className="bg-white p-6 rounded-md shadow-md w-1/2">
             <h1 className="text-3xl font-bold text-blue-500 mb-4">Welcome back to NBA Forum!</h1>
-              <input type="text" name="username" placeholder="Username" required className="border p-2 rounded-md mb-4 w-full" />
-              <input type="password" name="password" placeholder="Password" required className="border p-2 rounded-md mb-4 w-full" />
+              <input type="text" name="username" placeholder="Username" required className="border p-2 rounded-md mb-4 w-full" onChange={handleChange} />
+              <input type="password" name="password" placeholder="Password" required className="border p-2 rounded-md mb-4 w-full" onChange={handleChange} />
               <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded-md">SIGN IN</button>
             </form>
           </div>
