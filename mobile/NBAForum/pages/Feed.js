cb995ebbb98ee7058c6eb2ef1f7226ddb3064d19
zbuseaydin <zeynpaydinn@gmail.com>
2024-04-28 18:42:57 +0300

    add feed page with log out

---
+++ mobile/NBAForum/pages/Feed.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Context } from "../globalContext/globalContext.js"
+
+const Feed = () => {
+
+  const globalContext = useContext(Context)
+  const userObj = globalContext.userObj
+  console.log("user object:", userObj)
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, { color: 'blue' }]}>This is your feed!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+},
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        alignItems: 'center'
+    },
+    logoutButton: {
+      backgroundColor: '#007aff',
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+  },
+  logoutButtonText: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 16,
+  },
+});
+
+export default Feed;
