4e6e027d4fd8d82cdb6ca853b5980b00cbe2b446
Fatih Demir <101219059+fatih260@users.noreply.github.com>
2024-04-28 17:29:47 +0300

    backend/dockerfile added

--- backend/nba_project/settings.py
+++ backend/nba_project/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 import os
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv('./.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
