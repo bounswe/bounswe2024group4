bb3a7cb2454e1df348c099e49532088ea579a25b
zbuseaydin <zeynpaydinn@gmail.com>
2024-04-28 22:12:52 +0300

    add tab view

--- mobile/NBAForum/App.js
+++ mobile/NBAForum/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Provider } from "./globalContext/globalContext.js";
 import Navigator from './navigator.js'
+import Player from './pages/Player.js';
 
 
 export default function App() {
+    const Tab = createBottomTabNavigator();
 
     return (
         <Provider>
             <NavigationContainer>
-                <Navigator/>
+                <Tab.Navigator>
+                    <Tab.Screen name="Main" component={Navigator} options={{ headerShown: false }}/>
+                    <Tab.Screen name="Players" component={Player} />
+                </Tab.Navigator>
             </NavigationContainer>
         </Provider>
     );
