c8bd6ad07d711659af9f80a4fdf05c9840695f17
Nurullah UÇAN <130992676+nucan25@users.noreply.github.com>
2024-04-28 21:13:40 +0300

    Add Player Page on index.js

--- mobile/NBAForum/pages/index.js
+++ mobile/NBAForum/pages/index.js
@@ -2,12 +2,13 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import Team from "./Team";
 import HomePage from "./HomePage";
-
+import Player from "./Player";
 
 export{
     Login,
     SignUp,
     Team,
     HomePage,
+    Player,
 
 }
