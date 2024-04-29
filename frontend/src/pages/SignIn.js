41ac628966f9f9ec868307880e00c8ff5e19ddc3
berat-yilmaz <brtylmzx@gmail.com>
2024-04-29 03:15:51 +0300

    Change button gaps in sign in page in frontend

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -63,7 +63,7 @@ const SignIn = () => {
             <form onSubmit={handleSubmit} className="bg-white p-6 rounded-md shadow-md w-1/2">
             <h1 className="text-3xl font-bold text-blue-500 mb-4">Welcome to NBA Forum!</h1>
               <input type="text" name="username" placeholder="Username" required className="border p-2 rounded-md mb-4 w-full" onChange={handleChange} />
-              <input type="password" name="password" placeholder="Password" required className="border p-2 rounded-md mb-4 w-full" onChange={handleChange} />
+              <input type="password" name="password" placeholder="Password" required className="border p-2 rounded-md mb-8 w-full" onChange={handleChange} />
               { formErrorMessage !== '' && <p className="text-red-500 mb-4">{formErrorMessage}</p>}
               <div className="flex justify-center flex-col items-center">
                     <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold w-full px-4 py-2 rounded-md mb-2">Sign in</button>
