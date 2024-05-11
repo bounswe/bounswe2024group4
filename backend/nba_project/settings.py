4bbf6ea197941f466604fadf110dc281f2acd20c
Fatih Demir <101219059+fatih260@users.noreply.github.com>
2024-05-11 13:15:06 +0300

    database parameters

--- backend/nba_project/settings.py
+++ backend/nba_project/settings.py
@@ -96,6 +96,7 @@ WSGI_APPLICATION = 'nba_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
