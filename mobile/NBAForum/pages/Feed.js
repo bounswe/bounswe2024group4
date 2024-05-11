1d471c51e82c98baf9df0c4e04b2e5fcf690d92f
zbuseaydin <zeynpaydinn@gmail.com>
2024-05-11 14:23:25 +0300

    add create a post button

--- mobile/NBAForum/pages/Feed.js
+++ mobile/NBAForum/pages/Feed.js
@@ -1,16 +1,34 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import { Context } from "../globalContext/globalContext.js"
 
 const Feed = () => {
 
   const globalContext = useContext(Context)
   const userObj = globalContext.userObj
-  console.log("user object:", userObj)
+  const [posts, setPosts] = useState([]);
+//  console.log("user object:", userObj)
+
+
+  const handleCreatePost = () => {
+    // Navigate to the screen where users can create a new post
+  };
+
+  // Handler for liking a post
+  const handleLike = (postId) => {
+    // Implement logic to like the post
+  };
+
+  // Handler for commenting on a post
+  const handleComment = (postId) => {
+    // Implement logic to navigate to the screen where users can comment on the post
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={[styles.title, { color: 'blue' }]}>This is your feed!</Text>
+      <TouchableOpacity style={styles.createPostButton} onPress={handleCreatePost}>
+        <Text><Text style={styles.createPostButtonText}>Create a Post</Text></Text> 
+      </TouchableOpacity>
     </View>
   );
 };
@@ -18,15 +36,28 @@ const Feed = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
+    position: 'relative',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
     alignItems: 'center'
+  },
+  createPostButton: {
+    backgroundColor: "#1B64EB",
+    fontSize: 18,
+    padding: 16,
+    borderRadius: 16,
+    marginTop: 16,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  createPostButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
 
