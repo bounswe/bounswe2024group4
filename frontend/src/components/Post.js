6de0f94f8e469b7d530618e8ec9d337a4398c598
berat-yilmaz <brtylmzx@gmail.com>
2024-05-14 05:06:02 +0300

    Errors but get info from backend in profile

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -4,16 +4,21 @@ import {formatDistanceToNowStrict} from "date-fns";
 import { Context } from "../globalContext/globalContext.js";
 import axios from 'axios';
 
-function Post({ author, postId, createdAt, image }) {
-  const [content, setContent] = useState('');
-  const [imageURL, setImageURL] = useState('');
+function Post({ author, postId, createdAt, image, content }) {
+  // const [content, setContent] = useState('');
+  // const [imageURL, setImageURL] = useState('');
+  console.log(createdAt);
+  // const [createdAt, setCreatedAt] = useState('');
   const globalContext = useContext(Context);
   const { baseURL } = globalContext;
   const handleData = async () => {
     try {
-      const response = await axios.get(baseURL + '/post_detail/?post_id=' + postId);
-      setContent(response.data.post);
-      setImageURL(baseURL + response.data.image);
+      // const response = await axios.get(baseURL + '/post_detail/?post_id=' + postId);
+      // setContent(response.data.post);
+      // setImageURL(baseURL + response.data.image);
+      // setContent(content);
+      // setCreatedAt(createdAt);
+      // setImageURL(baseURL + image);
 
     } catch (error) {
       console.error('Error:', error);
@@ -28,17 +33,17 @@ function Post({ author, postId, createdAt, image }) {
 		<div className="border border-gray-300 rounded-md overflow-hidden mb-2">
 			<div className="bg-white shadow-md p-4 hover:bg-slate-50">
         <div className='flex gap-4'>
-          <img src={author.avatar} alt={author.name} className="w-12 h-12 rounded-full" />
+          {/* <img src={author.avatar} alt={author.name} className="w-12 h-12 rounded-full" /> */}
           <div className='w-full'>
             <div className="flex items-center mb-2 gap-2">
                 <div className="flex items-center gap-2">
-                    <h2 className="text-lg font-semibold">{author.name}</h2>
-                    <p className="text-gray-500 text-sm">{formatDistanceToNowStrict(createdAt, { addSuffix: true })}</p>
+                    {/* <h2 className="text-lg font-semibold">{author.name}</h2> */}
+                    <p className="text-gray-500 text-sm">{formatDistanceToNowStrict(new Date(createdAt), { addSuffix: true })}</p>
                 </div>
             </div>
             <p className="text-gray-800 mb-4">{content}</p> 
             {image && 
-                <img src={imageURL} alt="" className="rounded-3xl mb-4 object-cover min-w-96 max-h-[55vh]" />
+                <img src={image} alt="" className="rounded-3xl mb-4 object-cover min-w-96 max-h-[55vh]" />
             }
             <PostBottomBar />
           </div>
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -9,15 +9,24 @@ import { useParams } from 'react-router-dom';
 const Profile = () => {
   const [username, setUsername] = useState('');
   const [bio, setBio] = useState('');
+  const [followerCount, setFollowerCount] = useState(0);
+  const [followingCount, setFollowingCount] = useState(0);
+  const [profilePictureURL, setProfilePictureURL] = useState('');
   const [isFollowing, setIsFollowing] = useState(true);
+  const [posts, setPosts] = useState([]);
   const params = useParams();
 
   const handleData = async () => {
     try {
       setUsername(params.username);
-      const response = await axios.get(baseURL + '/profile_view_edit/' + username);
-      console.log(response.data.username);
-      
+      const response = await axios.get(baseURL + '/profile_view_edit/' + params.username);
+      console.log(response);
+      setBio(response.data.bio);
+      setProfilePictureURL(baseURL + response.data.profile_picture);
+      setFollowerCount(response.data.followers_count);
+      setFollowingCount(response.data.following_count);
+      setIsFollowing(response.data.isFollowing);
+      setPosts(response.data.posts);
 
     } catch (error) {
       console.error('Error:', error);
@@ -25,7 +34,6 @@ const Profile = () => {
   };
 
   const handleFollow = () => {
-    setIsFollowing(!isFollowing);
   }
 
   useEffect(() => {
@@ -37,30 +45,36 @@ const Profile = () => {
   return (
     <div className="bg-sky-50 min-h-screen">
       <Navbar />
-      <div class="flex container mx-auto ml-28 mt-[5vh] gap-8">
-        <div class="w-1/4 p-4 h-min">
-          <div class="bg-white p-6 rounded-2xl shadow h-full relative">
-            <img class="w-36 h-36 rounded-full mx-auto mb-4" src="https://hips.hearstapps.com/hmg-prod/images/r1g0dr-1-1670869236.jpg?crop=1.00xw:0.667xh;0,0.0811xh&resize=1200:*" alt=""/>
-                <p class="mx-auto font-extrabold text-center text-3xl mb-4"> {username} </p>
-                <p class="mb-8"> VERY QUOTATIOUS, I PERFORM RANDOM ACTS OF SHAQNESS VERY QUOTATIOUS, I PERFORM RANDOM ACTS OF SHAQNESS VERY QUOTATIOUS, I PERFORM RANDOM ACTS OF SHAQNESS VERY QUOTATIOUS, I PERFORM RANDOM ACTS OF SHAQNESS </p>
-                <div class="flex justify-center items-center gap-x-3 ml-4">
-                  <div class="flex flex-col items-center">
-                    <span class="font-bold">15.7M</span>
-                    <span class="font-light">Followers</span>
+      <div className="flex container mx-auto ml-28 mt-[5vh] gap-8">
+        <div className="w-1/4 p-4 h-min">
+          <div className="bg-white p-6 rounded-2xl shadow h-full relative">
+            <img className="w-36 h-36 rounded-full mx-auto mb-4" src={profilePictureURL} alt=""/>
+                <p className="mx-auto font-extrabold text-center text-3xl mb-4"> {username} </p>
+                <p className="mb-8"> {bio} </p>
+                <div className="flex justify-center items-center gap-x-3 ml-4">
+                  <div className="flex flex-col items-center">
+                    <span className="font-bold">{followerCount}</span>
+                    <span className="font-light">Followers</span>
                   </div>
-                  <div class="flex flex-col items-center">
-                    <span class="font-bold">50</span>
-                    <span class="font-light">Following</span>
+                  <div className="flex flex-col items-center">
+                    <span className="font-bold">{followingCount}</span>
+                    <span className="font-light">Following</span>
                   </div>
-                  <button class={`${isFollowing ? 'bg-white border text-blue-500' : 'bg-blue-500 text-white'} ${isFollowing ? 'hover:bg-slate-50' : 'hover:bg-blue-700'} font-bold min-w-24 py-2 rounded-lg mr-2`} onClick={handleFollow}>{isFollowing ? 'Following' : 'Follow'}</button>
+                  <button className={`${isFollowing ? 'bg-white border text-blue-500' : 'bg-blue-500 text-white'} ${isFollowing ? 'hover:bg-slate-50' : 'hover:bg-blue-700'} font-bold min-w-24 py-2 rounded-lg mr-2`} onClick={handleFollow}>{isFollowing ? 'Following' : 'Follow'}</button>
                 </div>
           </div>                 
         </div>
 
-        <div class="w-8/12 p-4 h-[80vh]">
-          <div class="bg-white p-6 rounded-2xl overflow-auto shadow h-full">
-            <h1 class="text-2xl font-bold mb-4"> Posts </h1>
-            <Post author={{ name: "shaq", avatar: "https://hips.hearstapps.com/hmg-prod/images/r1g0dr-1-1670869236.jpg?crop=1.00xw:0.667xh;0,0.0811xh&resize=1200:*"}} createdAt="2024-05-11T04:43:24.342858" image="https://pbs.twimg.com/media/Eb8RHOhWkAE9hcR?format=jpg&name=large" postId="3" />
+        <div className="w-8/12 p-4 h-[80vh]">
+          <div className="bg-white p-6 rounded-2xl overflow-auto shadow h-full">
+            <h1 className="text-2xl font-bold mb-4"> Posts </h1>
+            {posts.map((post) => (
+              <Post
+                content={post.content}
+                createdAt={post.createdAt}
+                image={baseURL + post.image}
+              />
+            ))}
           </div>
         </div>
       </div>
