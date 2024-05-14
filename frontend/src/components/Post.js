8284819d2e4d224f0b9d41a9e612b09b80fca4d5
berat-yilmaz <brtylmzx@gmail.com>
2024-05-14 22:57:38 +0300

    Likes & bookmark working

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,25 +1,86 @@
-import React, { useContext, useState, useEffect } from 'react';
-import PostBottomBar from './PostBottomBar';
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import { HeartIcon as HeartIconSolid, BookmarkIcon as BookmarkIconSolid } from "@heroicons/react/24/solid";
+import { HeartIcon as HeartIconOutline, ChatBubbleOvalLeftEllipsisIcon, BookmarkIcon as BookmarkIconOutline } from "@heroicons/react/24/outline";
 import {formatDistanceToNowStrict} from "date-fns";
 import { Context } from "../globalContext/globalContext.js";
 import axios from 'axios';
+import { InView } from 'react-intersection-observer';
+import Cookies from 'js-cookie';
 
-function Post({ author, postId, createdAt, image, content }) {
-  // const [content, setContent] = useState('');
-  // const [imageURL, setImageURL] = useState('');
-  console.log(createdAt);
-  // const [createdAt, setCreatedAt] = useState('');
+function Post({ postId }) {
+  const [content, setContent] = useState('');
+  const [imageURL, setImageURL] = useState('');
+  const [createdAt, setCreatedAt] = useState('');
+  const [author, setAuthor] = useState('');
+  const [isLiked, setIsLiked] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+
   const globalContext = useContext(Context);
   const { baseURL } = globalContext;
+
+  const handleLike = async () => {
+    // Store the previous values in case of reverting back
+    const previousLikesCount = likesCount;
+    const previousIsLiked = isLiked;
+    // Optimistically update the like state so we don't wait for the server
+    setLikesCount((prev) => isLiked ? prev - 1 : prev + 1);
+    setIsLiked((prev) => !prev);
+    const config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+    };
+  
+    try {
+      const response = await axios.post(baseURL + '/like_or_unlike_post/' + postId, '', config);
+      if (response.status !== 200) {
+        // Like is not accepted by the server, revert back
+        setLikesCount(previousLikesCount);
+        setIsLiked(previousIsLiked);
+      }
+    } catch (error) {
+      console.error('Error liking the post:', error);
+    }
+  };
+
+  const handleBookmark = async () => {
+    // Store the previous value in case of reverting back
+    const previousBookmarked = isBookmarked;
+    // Optimistically update the bookmark state so we don't wait for the server
+    setIsBookmarked((prev) => !prev);
+    const config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+    };
+  
+    try {
+      const response = await axios.post(baseURL + '/bookmark_or_unbookmark_post/' + postId, '', config);
+      if (response.status !== 200) {
+        // Bookmark is not accepted by the server, revert back
+        setIsBookmarked(previousBookmarked);
+      }
+    } catch (error) {
+      console.error('Error liking the post:', error);
+    }
+  };
+
   const handleData = async () => {
     try {
-      // const response = await axios.get(baseURL + '/post_detail/?post_id=' + postId);
-      // setContent(response.data.post);
-      // setImageURL(baseURL + response.data.image);
-      // setContent(content);
-      // setCreatedAt(createdAt);
-      // setImageURL(baseURL + image);
-
+      const response = await axios.get(baseURL + '/post_detail/' + postId + '/');
+      console.log(response);
+      setContent(response.data.post);
+      setImageURL(baseURL + response.data.image);
+      setCreatedAt(formatDistanceToNowStrict(response.data.created_at, { addSuffix: true }));
+      setIsLiked(response.data.user_has_liked);
+      setLikesCount(response.data.likes_count);
+      setIsBookmarked(response.data.user_has_bookmarked);
+      setAuthor(response.data.username);
     } catch (error) {
       console.error('Error:', error);
     }
@@ -27,7 +88,7 @@ function Post({ author, postId, createdAt, image, content }) {
 
   useEffect(() => {
     handleData();
-  }, []);
+  }, [postId])
 
 	return (
 		<div className="border border-gray-300 rounded-md overflow-hidden mb-2">
@@ -37,15 +98,53 @@ function Post({ author, postId, createdAt, image, content }) {
           <div className='w-full'>
             <div className="flex items-center mb-2 gap-2">
                 <div className="flex items-center gap-2">
-                    {/* <h2 className="text-lg font-semibold">{author.name}</h2> */}
-                    <p className="text-gray-500 text-sm">{formatDistanceToNowStrict(new Date(createdAt), { addSuffix: true })}</p>
+                    <h2 className="text-lg font-semibold">{author}</h2>
+                    <p className="text-gray-500 text-sm">{createdAt}</p>
                 </div>
             </div>
             <p className="text-gray-800 mb-4">{content}</p> 
-            {image && 
-                <img src={image} alt="" className="rounded-3xl mb-4 object-cover min-w-96 max-h-[55vh]" />
+            {imageURL && 
+                <img src={imageURL} alt="" className="rounded-3xl mb-4 object-cover min-w-96 max-h-[55vh]" />
             }
-            <PostBottomBar />
+            <div className="flex items-center relative">
+            <div className="group flex items-center mr-4">
+                {isLiked ? (
+                    <HeartIconSolid 
+                        className="w-6 h-6 text-red-500 mr-1 cursor-pointer group-hover:text-red-500" 
+                        onClick={handleLike} 
+                    />
+                ) : (
+                    <HeartIconOutline 
+                        className="w-6 h-6 text-gray-500 mr-1 cursor-pointer rounded-full group-hover:text-red-500 group-hover:bg-red-100" 
+                        onClick={handleLike}
+                    />
+                )}
+                <span className={`${isLiked ? 'text-red-500' : 'text-gray-500 group-hover:text-red-500'}`}>
+                    {likesCount}
+                </span>
+            </div>
+            <div className="group flex items-center mr-4">
+                <ChatBubbleOvalLeftEllipsisIcon
+                    className="w-6 h-6 text-gray-500 mr-1 cursor-pointer rounded-full group-hover:text-blue-500 group-hover:bg-blue-100"
+                />
+                <span className="text-gray-600 group-hover:text-blue-500">
+                    {/* {commentCount} */}
+                </span>
+            </div>
+            <div className="group flex items-center mr-4 absolute right-0">
+                {isBookmarked ? (
+                    <BookmarkIconSolid 
+                        className="w-6 h-6 text-blue-500 mr-1 cursor-pointer group-hover:text-blue-500" 
+                        onClick={handleBookmark} 
+                    />
+                ) : (
+                    <BookmarkIconOutline 
+                        className="w-6 h-6 text-gray-500 mr-1 cursor-pointer rounded-full object-left group-hover:text-blue-500 group-hover:bg-blue-100" 
+                        onClick={handleBookmark}
+                    />
+                )}
+            </div>
+          </div>
           </div>
         </div>
       </div>
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -6,21 +6,21 @@ import axios from 'axios';
 import { Context } from "../globalContext/globalContext.js";
 import { useParams } from 'react-router-dom';
 
+
 const Profile = () => {
   const [username, setUsername] = useState('');
   const [bio, setBio] = useState('');
   const [followerCount, setFollowerCount] = useState(0);
   const [followingCount, setFollowingCount] = useState(0);
   const [profilePictureURL, setProfilePictureURL] = useState('');
-  const [isFollowing, setIsFollowing] = useState(true);
+  const [isFollowing, setIsFollowing] = useState(false);
   const [posts, setPosts] = useState([]);
   const params = useParams();
 
   const handleData = async () => {
     try {
       setUsername(params.username);
-      const response = await axios.get(baseURL + '/profile_view_edit/' + params.username);
-      console.log(response);
+      const response = await axios.get(baseURL + '/profile_view_edit_others/' + params.username);
       setBio(response.data.bio);
       setProfilePictureURL(baseURL + response.data.profile_picture);
       setFollowerCount(response.data.followers_count);
@@ -34,6 +34,7 @@ const Profile = () => {
   };
 
   const handleFollow = () => {
+
   }
 
   useEffect(() => {
@@ -47,11 +48,11 @@ const Profile = () => {
       <Navbar />
       <div className="flex container mx-auto ml-28 mt-[5vh] gap-8">
         <div className="w-1/4 p-4 h-min">
-          <div className="bg-white p-6 rounded-2xl shadow h-full relative">
-            <img className="w-36 h-36 rounded-full mx-auto mb-4" src={profilePictureURL} alt=""/>
+          <div className="bg-white p-6 rounded-2xl shadow h-full items-center">
+            <img className="w-36 h-36 rounded-full mx-auto mb-4 border border-slate-150" src={profilePictureURL} alt=""/>
                 <p className="mx-auto font-extrabold text-center text-3xl mb-4"> {username} </p>
-                <p className="mb-8"> {bio} </p>
-                <div className="flex justify-center items-center gap-x-3 ml-4">
+                {bio && <p className="mb-8">{bio}</p>}
+                <div className="flex justify-center items-center gap-x-3 mx-auto mb-3">
                   <div className="flex flex-col items-center">
                     <span className="font-bold">{followerCount}</span>
                     <span className="font-light">Followers</span>
@@ -60,8 +61,8 @@ const Profile = () => {
                     <span className="font-bold">{followingCount}</span>
                     <span className="font-light">Following</span>
                   </div>
-                  <button className={`${isFollowing ? 'bg-white border text-blue-500' : 'bg-blue-500 text-white'} ${isFollowing ? 'hover:bg-slate-50' : 'hover:bg-blue-700'} font-bold min-w-24 py-2 rounded-lg mr-2`} onClick={handleFollow}>{isFollowing ? 'Following' : 'Follow'}</button>
                 </div>
+                <button className={`${isFollowing ? 'bg-white border text-blue-500' : 'bg-blue-500 text-white'} ${isFollowing ? 'hover:bg-slate-50' : 'hover:bg-blue-700'} font-bold py-2 w-full mx-auto rounded-lg`} onClick={handleFollow}>{isFollowing ? 'Following' : 'Follow'}</button>
           </div>                 
         </div>
 
@@ -70,9 +71,8 @@ const Profile = () => {
             <h1 className="text-2xl font-bold mb-4"> Posts </h1>
             {posts.map((post) => (
               <Post
-                content={post.content}
-                createdAt={post.createdAt}
-                image={baseURL + post.image}
+                key={post.post_id}
+                postId={post.post_id}
               />
             ))}
           </div>
