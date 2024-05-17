89364b546a6a82a2ab18a61ae233856ed47fe80a
Bilge Kaan Güneyli <75316186+kaanguneyli@users.noreply.github.com>
2024-05-17 06:12:20 +0300

    Update settings.py

--- backend/nba_project/settings.py
+++ backend/nba_project/settings.py
@@ -39,6 +39,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
+    'DEFAULT_GENERATOR_CLASS': 'drf_yasg.generators.OpenAPISchemaGenerator',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
