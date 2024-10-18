6f46622680797607514dd024dc4c955dc48485d6
zbuseaydin <zeynpaydinn@gmail.com>
2024-10-19 01:01:39 +0300

    use tailwind

--- frontend/fitness-diet-forum/src/App.js
+++ frontend/fitness-diet-forum/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
+import Meal from './pages/Meal'; // Import Meal component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-900 min-h-screen">
+      <Meal /> {/* Render the list of meals */}
     </div>
   );
 }
