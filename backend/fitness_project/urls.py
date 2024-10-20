9b7e4eb856932208c0400afb275af128a24bb8a8
Can <mcank2000@gmail.com>
2024-10-20 19:37:42 +0300

    url structure finalized on fitness_project

--- backend/fitness_project/urls.py
+++ backend/fitness_project/urls.py
@@ -16,24 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from exercise_program_app import views
+from exercise_program_app import views as exercise_program_views
 from user_auth_app import views as auth_views
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('posts/', include('posts_app.urls')),
-    path('profiles/', include('profiles_app.urls')),
-    #path('programs/', include('programs_app.urls')),
-    path('feed/', include('social_feed_app.urls')),
-    path('messages/', include('messaging_app.urls')),
-    path('search/', include('search_app.urls')),
-    path('auth/', include('user_auth_app.urls')),
-    path('diet-program/', include('diet_program_app.urls')),
-    path('exercise-program/', include('exercise_program_app.urls')),
-
-    path('workout_program/', views.workout_program, name='workout_program'),
-    path('get_exercises/', views.get_exercises, name='get_exercises'),
+    path('admin/', admin.site.urls),
+    path('workout_program/', exercise_program_views.workout_program, name='workout_program'),
+    path('get_exercises/', exercise_program_views.get_exercises, name='get_exercises'),
     path('sign_up/', auth_views.sign_up, name='sign_up'),
     path('log_in/', auth_views.log_in, name='log_in'),
     path('log_out/', auth_views.log_out, name='log_out'),
