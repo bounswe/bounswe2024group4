a357cba8ee30ce552f807a61526c3f4f61268665
talhaordukaya <talha.ordukaya@hotmail.com>
2024-11-19 23:28:53 +0300

    Added post creation.

--- backend/posts_app/models.py
+++ backend/posts_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from user_auth_app.models import User
 
-# Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    exerciseId = models.IntegerField(null=True)
+    mealId = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.content
+    
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.content
--- backend/posts_app/views.py
+++ backend/posts_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from posts_app.models import Post
 
-# Create your views here.
+def post(request):
+    if request.method == 'POST':
+        user = request.user
+        content = request.POST.get('content')
+        exerciseId = request.POST.get('exerciseId')
+        mealId = request.POST.get('mealId')
+        post = Post.objects.create(user=user, content=content, exerciseId=exerciseId, mealId=mealId)
+        return JsonResponse({'message': 'Post created successfully', 'post_id': post.id}, status=201)
+    return render(request, 'create_post.html')
