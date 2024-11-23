060fe9517ac440bdf63fbb865ab1441e2f20a352
zbuseaydin <zeynpaydinn@gmail.com>
2024-11-23 20:24:11 +0300

    implement log out

--- frontend/fitness-diet-forum/src/components/Topbar.js
+++ frontend/fitness-diet-forum/src/components/Topbar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useContext } from "react";
 import { FaSearch } from 'react-icons/fa';
+import { Context } from "../globalContext/globalContext.js";
+import axios from 'axios';
+import { setLoggedIn } from "../components/Auth.js";
+import { useNavigate } from "react-router-dom";  
 
 const Topbar = () => {
+  // This function will be called when the logout button is clicked
+  const globalContext = useContext(Context);
+  const { baseURL } = globalContext;
+  const navigate = useNavigate();  
+
+  const handleLogout = async () => {
+    try {
+      await axios.get(baseURL + "/log_out/");
+      setLoggedIn("false");
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="bg-black text-white p-4 flex justify-between items-center">
       <div className="flex-1 flex justify-center items-center">
@@ -14,16 +33,19 @@ const Topbar = () => {
           />
         </div>
       </div>
-      <button className="bg-black text-white p-2 rounded border-2 border-blue-500 shadow-lg transition-shadow duration-300 hover:shadow-blue-500 hover:shadow-lg hover:scale-105">
-        Create Post
-      </button>
+      <div className="flex items-center gap-4">
+        <button className="bg-black text-white p-2 rounded border-2 border-blue-500 shadow-lg transition-shadow duration-300 hover:shadow-blue-500 hover:shadow-lg hover:scale-105">
+          Create Post
+        </button>
+        <button 
+          onClick={handleLogout} 
+          className="bg-red-500 text-white p-2 rounded border-2 border-red-700 shadow-lg transition-shadow duration-300 hover:shadow-red-500 hover:shadow-lg hover:scale-105"
+        >
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Topbar;
-
-
-
-
-
