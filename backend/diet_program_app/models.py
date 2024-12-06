8337b6a462f23dc3db4463f565bb4514b9278467
Bilge Kaan Güneyli <bilgekaanguneyli@gmail.com>
2024-12-07 00:09:54 +0300

    define models

--- backend/diet_program_app/models.py
+++ backend/diet_program_app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from user_auth_app.models import User
 
-# Create your models here.
+
+class Food(models.Model):
+    food_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    # calories = models.FloatField()
+    # protein = models.FloatField()
+    # carbs = models.FloatField()
+    # fat = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.food_name
+
 class Meal(models.Model):
     meal_id = models.AutoField(primary_key=True)
     meal_name = models.CharField(max_length=50)
@@ -8,6 +21,7 @@ class Meal(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     rating = models.FloatField(default=0)
     rating_count = models.IntegerField(default=0)
+    foods = models.ManyToManyField(Food)
 
     def _str_(self):
         return self.meal_name
--- backend/diet_program_app/views.py
+++ backend/diet_program_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+import json
+from views import Meal
 # Create your views here.
+def create_meal(request):
+    if request.method == 'POST':
+        # create a new meal
+        user = request.user
+        data = json.loads(request.body)
+        meal_name = data['meal_name']
+        foods = data['foods']
+        # create a new meal
+        # meal = Meal.objects.create(meal_name=meal_name, created_by=user)
