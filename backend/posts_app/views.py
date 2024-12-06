3c9ef720310497a1ee7e76fcae833d6342f1fa05
zbuseaydin <zeynpaydinn@gmail.com>
2024-12-06 23:59:21 +0300

    get username as parameter

--- backend/posts_app/views.py
+++ backend/posts_app/views.py
@@ -13,11 +13,12 @@ from swagger_docs.swagger import post_schema, toggle_like_schema, comment_schema
 @csrf_exempt
 def post(request):
     if request.method == 'POST':
-        user = request.user
-        # user = User.objects.get(username=request.POST.get('username'))
-        content = request.data.get('content')
-        workoutId = request.data.get('workoutId')
-        mealId = request.data.get('mealId')
+        #user = request.user
+        data = request.data
+        user = User.objects.get(username=data.get('username'))
+        content = data.get('content')
+        workoutId = data.get('workoutId')
+        mealId = data.get('mealId')
         post = Post.objects.create(user=user, content=content)
         
         if workoutId:
