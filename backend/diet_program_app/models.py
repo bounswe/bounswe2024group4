b2c5c7a60a8f1c6584949ed1c43891a45ccb7df3
Bilge Kaan Güneyli <bilgekaanguneyli@gmail.com>
2024-12-06 23:27:02 +0300

    start meal

--- backend/diet_program_app/models.py
+++ backend/diet_program_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Meal(models.Model):
+    meal_id = models.AutoField(primary_key=True)
+    meal_name = models.CharField(max_length=50)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField(default=0)
+    rating_count = models.IntegerField(default=0)
+
+    def _str_(self):
+        return self.meal_name
