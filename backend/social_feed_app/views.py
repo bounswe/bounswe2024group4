3da18bd895aa44a1ba4e13dd2cde3f3adbed6d7d
talhaordukaya <talha.ordukaya@hotmail.com>
2024-12-09 09:32:36 +0300

    Updated feed related endpoints for new auth system.

--- backend/social_feed_app/views.py
+++ backend/social_feed_app/views.py
@@ -4,10 +4,14 @@ from django.views.decorators.csrf import csrf_exempt
 from user_auth_app.models import User
 from drf_yasg.utils import swagger_auto_schema
 from swagger_docs.swagger import feed_schema, following_feed_schema
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 @swagger_auto_schema(method='get', **feed_schema)
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def feed(request):
     if request.method == 'GET':
@@ -19,11 +23,12 @@ def feed(request):
 
 @swagger_auto_schema(method='get', **following_feed_schema)
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def following_feed(request):
     if request.method == 'GET':
-        # user = request.user
-        user = User.objects.get(username=request.GET.get('username'))
+        user = request.user
         following = user.following.all()
         posts = Post.objects.filter(user__in=following).values()
         return JsonResponse({'posts': list(posts)}, status=200)
