aeb9dc7dcae1cd935b50d96a3a5f97a37fedf8fa
batuhancanli0 <batuhan02bc@gmail.com>
2024-12-16 15:17:28 +0300

    debug prints removed

--- backend/exercise_program_app/views.py
+++ backend/exercise_program_app/views.py
@@ -505,11 +505,9 @@ def workout_log(request, workout_id):
         if 'workout_completed' in data:
             workout_log.is_completed = data['workout_completed']
             workout_log.save()
-        print(data)
         # Handle exercise logs
         if 'exercises' in data['exercises']:
             exercise_ids = [ex['exercise_id'] for ex in data['exercises']]
-            print("ex ids:",exercise_ids)
             workout_exercises = Exercise.objects.filter(
                 workout=workout, 
                 exercise_id__in=exercise_ids
@@ -519,9 +517,7 @@ def workout_log(request, workout_id):
                 return JsonResponse({'error': 'Some exercises do not belong to this workout'}, status=400)
                 
             for exercise_data in data['exercises']:
-                print("ex data:",exercise_data)
                 exercise = workout_exercises.get(exercise_id=exercise_data['exercise_id'])
-                print("exercise:")
                 exercise_log, _ = ExerciseLog.objects.get_or_create(
                     workout_log=workout_log,
                     exercise=exercise
@@ -533,9 +529,6 @@ def workout_log(request, workout_id):
                 exercise_log.actual_reps = exercise_data.get('actual_reps', 0)
                 exercise_log.weight = exercise_data.get('weight', 0.0)
                 exercise_log.save()
-
-        else:
-            print("no exercise")
                 
         return JsonResponse({
             'message': 'Workout log updated successfully',
